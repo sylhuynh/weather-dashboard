f14740a1c602d104e50db2a7f85b7cf379b1b0fe
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-03-22 11:51:41 -0700

    starter code + pseudo code for weather dashboard

---
+++ script.js
@@ -0,0 +1,41 @@
+// when user clicks on the search button
+    // prevent the btn from trying to submit the form
+    // get the value of the input field and assign to variable
+    // add searchTerm to the queryUrl 
+
+// use ajax call to retrieve data from weather API with users input
+    // create var for the response if need be (ex. response.data/ response.docs)
+    // create response var for currenttemperature, currenthumidity, wind speed, UV index, currentDate, currentweathercasticon
+
+    //create 1 bootstrap card that loops through the 5 day forecast (bands activity 11 & activity 13 unit 6)
+        // create a container
+        // create div with card class and class that changes background color to blue
+            // create h1 with date and append to the div
+            // create h2 with temp and append to the div
+            // create h3 with humidity and append to the div
+    
+    // search history list
+        // jQuery listen to document activity
+        // document.addeventListener("click", INSERT CITY NAME, INSERT FUNCTION NAME)
+        // need to get from local storage
+
+
+// GIVEN a weather dashboard with form inputs
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
