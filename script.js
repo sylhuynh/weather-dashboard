b785850ec1219bbaff6439722ae7380a7312d30e
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-03-22 18:09:37 -0700

    added in the weather icon and index value for searched city

--- script.js
+++ script.js
@@ -1,49 +1,66 @@
 var apiKey = "84a64c01cc6ad7d818bbc8727e5ba52f";
 
 // create function that searches for current weather 
-function searchCurrentWeather(city){
-    
-//add searchTerm to the queryUrl 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+function searchCurrentWeather(city) {
 
-// use ajax call to retrieve data from weather API with users input
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    //printing the entire object to the console
-    console.log(response);
-    
-    // constructing HTML containing current weather information
-    // create response var for temperature, humidity, wind speed, UV index, date, icon
-        var cityName = $("<h2>").text(response.name + " " + moment().format('L'));
-        var currentWeatherIcon = $("h2").text(response.weather.icon);
-        var currentTemp = $("<p>").text("Temperature: " + response.main.temp + " °F");
-        var currentHumidity = $("<p>").text("Humidity: " + response.main.humidity + " %");
-        var currentWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
+    //add searchTerm to the queryUrl 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-    //empty the contents of the card body, append new weather content to display on page
-    $(".card-body").empty();
-    $(".card-body").append(cityName, currentWeatherIcon, currentTemp, currentHumidity, currentWindSpeed);
-});
-}   
+    // use ajax call to retrieve data from weather API with users input
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        //printing the entire object to the console
+        console.log(response);
 
-    //create 1 bootstrap card that loops through the 5 day forecast (bands activity 11 & activity 13 unit 6)
-        // create a container
-        // create div with card class and class that changes background color to blue
-            // create h1 with date and append to the div
-            // create h2 with temp and append to the div
-            // create h3 with humidity and append to the div
-    
-    // search history list
-        // jQuery listen to document activity
-        // document.addeventListener("click", INSERT CITY NAME, INSERT FUNCTION NAME)
-        // need to get from local storage
+        // use ajax call to retrieve uv index data
+        var indexUVURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+        $.ajax({
+            url: indexUVURL,
+            method: "GET"
+        }).then(function (UVIndex) {
+
+            // constructing HTML containing current weather information
+            // create response var for temperature, humidity, wind speed, UV index, date, icon
+            var cityName = $("<h2>").text(response.name + " " + moment().format('L'));
+
+            //retrieve icon data
+            var currentWeatherIcon = response.weather[0].icon;
+            // add to new img element and attach to icon url
+            var newImageIcon = $("<img>");
+                newImageIcon.attr("src", "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png");
+
+            var currentTemp = $("<p>").text("Temperature: " + response.main.temp + " °F");
+            var currentHumidity = $("<p>").text("Humidity: " + response.main.humidity + " %");
+            var currentWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
+            var currentUVIndex = $("<p>").text("UV Index: " + UVIndex.value);
+
+            //empty the contents of the card body, append new weather content to display on page
+            $(".card-body").empty();
+            $(".card-body").append(cityName, currentTemp, currentHumidity, currentWindSpeed, currentUVIndex);
+            // append icon to city name and date 
+            $("h2").append(newImageIcon);
+        });
+    });
+}
+
+//create 1 bootstrap card that loops through the 5 day forecast (bands activity 11 & activity 13 unit 6)
+// create a container
+// create div with card class and class that changes background color to blue
+// create h1 with date and append to the div
+// create h2 with temp and append to the div
+// create h3 with humidity and append to the div
+
+// search history list
+// jQuery listen to document activity
+// document.addeventListener("click", INSERT CITY NAME, INSERT FUNCTION NAME)
+// need to get from local storage
 
 
 
 // when user clicks on the search button
-$("#search-btn").on("click", function(event){
+$("#search-btn").on("click", function (event) {
     // prevent th btn from trying to submit the form
     event.preventDefault();
     // get the value of the input field and assign to variable
